2755711262abe0a3c9510c2a2e9f27078657dab5
Illya Holubiev <121903042+HolubievIllya@users.noreply.github.com>
2023-12-29 23:02:18 +0200

    Create Exercises.py

---
+++ PythonBasic/Exercises.py
@@ -0,0 +1,50 @@
+# Task 1
+
+# Division
+print(5 / 8)
+
+#____
+print(7 + 10)
+
+# Addition
+print(4+5)
+
+# Subtraction
+print(5-5)
+
+# Multiplication
+print(3*5)
+
+# Division
+print(10/2)
+
+# Task 2
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
+
+# Create the variables monthly_savings and num_months
+monthly_savings = 10
+num_months = 4
+
+# Multiply monthly_savings and num_months
+new_savings = monthly_savings * num_months
+
+# Add new_savings to your savings
+total_savings = new_savings + savings
+
+# Print total_savings
+print(total_savings)
+
+# Create a variable half
+half = .5
+
+# Create a variable intro
+inttro = "Hello! How are you?"
+
+# Create a variable is_good
+is_good = True
+
