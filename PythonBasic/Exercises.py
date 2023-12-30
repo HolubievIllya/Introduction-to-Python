c58529ca424ef9566003529fcd2f4999b8cc09f6
Illya Holubiev <121903042+HolubievIllya@users.noreply.github.com>
2023-12-30 13:43:15 +0200

    Update Exercises.py

--- PythonBasic/Exercises.py
+++ PythonBasic/Exercises.py
@@ -48,5 +48,32 @@ inttro = "Hello! How are you?"
 # Create a variable is_good
 is_good = True
 
+monthly_savings = 10
+num_months = 12
+intro = "Hello! How are you?"
 
+# Calculate year_savings using monthly_savings and num_months
+year_savings = monthly_savings * num_months
+
+# Print the type of year_savings
+print(type(year_savings))
+
+# Assign sum of intro and intro to doubleintro
+doubleintro = intro + intro
+
+# Print out doubleintro
+print(doubleintro)
+
+# Definition of savings and total_savings
+savings = 100
+total_savings = 150
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(total_savings) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
 
